0a988df6c0edc9d81519704c44d485ceb4fbba84
Jacek Achtelik <jachtelik@edokumenty.eu>
2024-01-16 07:49:08 +0100

    Dodanie composer-a

---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+
+// ini_set('memory_limit','512M');
+// echo ini_get('memory_limit');
+// phpinfo();
+
+$app = require_once __DIR__ . "/../src/App/bootstrap.php";
+$app->run();
---
+++ src/App/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+require_once __DIR__ . "/../Framework/App.inc";
+
+use Framework\App;
+
+$app = new App();
+
+return $app;
